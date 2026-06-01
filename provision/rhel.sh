b0bbfbec770608406985703d35dcdcc3146b6ede
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2026-06-01 00:29:35 -0300

    fix: proper error handling
    - Also making sure the system is up to date, without breaking the
      already installed Virtualbox Guest Additions module

--- provision/rhel.sh
+++ provision/rhel.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-# Copia a chave SSH
+set -eo pipefail
+
 mkdir -p /root/.ssh
-cp /vagrant/files/key /root/.ssh/id_rsa
-cp /vagrant/files/key.pub /root/.ssh/id_rsa.pub
-cp /vagrant/files/key.pub /root/.ssh/authorized_keys
-chmod 400 /root/.ssh/*
+
+if [[ -d /vagrant ]]
+then
+    for file in /vagrant/files/key /vagrant/files/key.pub /vagrant/files/key.pub
+    do
+        if [[ -f $file ]]
+        then
+            cp -v $file /root/.ssh/
+        fi
+    done
+    chmod 400 /root/.ssh/*
+fi
+
+# O Alma Linux requer correções no instalador do Virtualbox Guest Additions para
+# que este simplesmente funcione. Manter o kernel atual (que já vem com o Guest
+# Additions instalado) vai evitar muita dor de cabeça
+echo 'exclude=kernel*' >> /etc/dnf/dnf.conf
+
+dnf makecache && dnf update -y && dnf install epel-release -y && dnf makecache && \
+    dnf clean packages
