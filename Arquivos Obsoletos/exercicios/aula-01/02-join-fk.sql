1ad32bb50794643eb84ca6e332588f0ab076ba8d
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2026-06-25 18:26:01 -0300

    chore: lixo

--- Arquivos Obsoletos/exercicios/aula-01/02-join-fk.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE usuarios_infos (usuario_id PRIMARY KEY, sexo ENUM('M','F'), curriculo TEXT);
-
-ALTER TABLE usuarios_infos ADD CONSTRAINT fk_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id);
--- Arquivos Obsoletos/files/4linux/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-apt-get update && apt-get install -y libdata-faker-perl libclass-dbi-mysql-perl libtext-lorem-perl libdata-faker-perl
